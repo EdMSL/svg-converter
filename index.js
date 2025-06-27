a9e79a80b215d8ac31a96f14f1ae0ce4917f0931
Eduard Trait <eduard2011rus@gmail.com>
2025-06-28 00:06:08 +0700

    Update

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 // Если запускать из папки проекта, то выполняем "node ./index.js путь_к_папке"
 // Чтобы выполнить скрипт  сразу из папки с файлами, то выполняем прсто "convertsvg"
-import { vectorize, ColorMode, Hierarchical, PathSimplifyMode, Preset } from '@neplex/vectorizer';
-import {trace} from 'potrace';
+import * as vectorizer from '@neplex/vectorizer';
+import { trace } from 'potrace';
 import { readFile, writeFile } from 'node:fs/promises';
 import fs from 'fs';
 import path from 'path';
 import { optimize } from 'svgo';
 
 const destPath = path.resolve(process.argv[2] || "./");
-const convertFolderPath = path.join(destPath, "svg");
-
-if (!fs.existsSync(convertFolderPath)) {
-  fs.mkdirSync(convertFolderPath)
-}
 
 const convertFile = file => {
   return new Promise((resolve, reject) => {
@@ -23,32 +18,42 @@ const convertFile = file => {
   })
 }
 
-fs.readdirSync(destPath).forEach(async (file, index) => {
-  const pathToFile = path.join(destPath, file);
+const convertFiles = (pathTo) => {
+  fs.readdirSync(pathTo).forEach(async (file, index) => {
+    const pathToFile = path.join(pathTo, file);
+    const convertFolderPath = path.join(pathTo, "svg");
 
-  if (fs.lstatSync(pathToFile).isFile()) {
-    // Вариант 1. Для цветных изображений.
-    // const src = ;
-    const svg = await vectorize(await readFile(pathToFile), {
-      colorMode: ColorMode.Color,
-      colorPrecision: 6,
-      filterSpeckle: 4,
-      spliceThreshold: 35,
-      cornerThreshold: 40,
-      hierarchical: Hierarchical.Stacked,
-      mode: PathSimplifyMode.Spline,
-      layerDifference: 5,
-      lengthThreshold: 5,
-      maxIterations: 10,
-      pathPrecision: 8,
-    });
+    if (fs.lstatSync(pathToFile).isFile() && path.extname(pathToFile) === '.png') {
+      // Вариант 1. Для цветных изображений.
+      const svg = await vectorizer.vectorize(await readFile(pathToFile), {
+        colorMode: vectorizer.ColorMode.Color,
+        colorPrecision: 6,
+        filterSpeckle: 4,
+        spliceThreshold: 35,
+        cornerThreshold: 40,
+        hierarchical: vectorizer.Hierarchical.Stacked,
+        mode: vectorizer.PathSimplifyMode.Spline,
+        layerDifference: 5,
+        lengthThreshold: 5,
+        maxIterations: 10,
+        pathPrecision: 8,
+      });
 
-    // Вариант 2. Для ч\б изображений.
-    // const svg = await convertFile(pathToFile);
+      // Вариант 2. Для ч\б изображений.
+      // const svg = await convertFile(pathToFile);
 
-    // Процесс обработки.
-    const result = optimize(svg);
-  
-    await writeFile(`${path.resolve(convertFolderPath, path.basename(file, 'png'))}svg`, result.data);
-  }
-})
+      // Процесс обработки.
+      const result = optimize(svg);
+
+      if (result.data && !fs.existsSync(convertFolderPath)) {
+        fs.mkdirSync(convertFolderPath)
+      }
+
+      await writeFile(`${path.resolve(convertFolderPath, path.basename(file, 'png'))}svg`, result.data);
+    } else if (fs.lstatSync(pathToFile).isDirectory() && path.basename(pathToFile) !== 'svg') {
+      convertFiles(pathToFile);
+    }
+  })
+}
+
+convertFiles(destPath);
